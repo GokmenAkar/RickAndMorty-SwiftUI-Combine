c5be5950e64212e62a6896146a4652804440edda
GokmenAkar <gokmen.akar92@gmail.com>
2020-03-23 14:19:24 +0300

    Initial Commit

---
+++ RickAndMorty-SwiftUI-Combine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RickAndMorty-SwiftUI-Combine
+//
+//  Created by Developer on 23.03.2020.
+//  Copyright © 2020 Developer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
